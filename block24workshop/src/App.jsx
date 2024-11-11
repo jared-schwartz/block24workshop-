7f6cfd4f2f22d130bf10efc1d4fa3a8cd07d03b8
Jared Schwartz <jareds1070@gmail.com>
2024-11-10 22:26:22 -0600

    firstcommit

--- block24workshop/src/App.jsx
+++ block24workshop/src/App.jsx
@@ -1,15 +1,37 @@
-import './App.css'
+import './App.css';
+import {puppyList} from './data.js';
+import { useState } from 'react';
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick(id) {
+    setFeatPupId(id);
+  }
+
+  // Find the featured puppy based on featPupId
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
-    <>
-      <div>
-      <h1>Congrats you made it this far!</h1>
-      </div>
-
-    </>
-  )
+    <div className="App">
+      {puppies.map((puppy) => (
+        <p onClick={() => handleClick(puppy.id)} key={puppy.id}>
+          {puppy.name}
+        </p>
+      ))}
+      
+      {featPupId && featuredPup && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
